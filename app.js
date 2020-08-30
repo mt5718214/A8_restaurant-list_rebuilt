28059cb80850d27fadb017de5e73ae0c127587e6
Lin <mt5718214@gmail.com>
2020-08-30 15:35:49 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -53,6 +53,14 @@ app.post('/restaurants/new', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(3000, () => {
   console.log('Express is running on http://localhost:3000')
 })
