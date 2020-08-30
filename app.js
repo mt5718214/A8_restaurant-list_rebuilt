a86523810f78a55d0613f37642a16208877d90e5
Lin <mt5718214@gmail.com>
2020-08-30 12:27:06 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express()
 const mongoose = require('mongoose')
 const db = mongoose.connection
 
+const exphbs = require('express-handlebars')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 db.on('error', () => {
@@ -15,7 +20,7 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('this is restaurantList')
+  res.render('index')
 })
 
 
