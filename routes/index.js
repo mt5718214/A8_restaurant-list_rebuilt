b70950890c98eb5f15566fa5273d86fc55f7bb8e
Lin <mt5718214@gmail.com>
2020-11-08 11:34:23 +0800

    feat: add users modules

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
 const search = require('./modules/search')
 const sort = require('./modules/sort')
+const users = require('./modules/users')
 
-route.use('/', home)
+route.use('/users', users)
 route.use('/restaurants', restaurant)
 route.use('/search', search)
 route.use('/sort', sort)
+route.use('/', home)
 
 module.exports = route
