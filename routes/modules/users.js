05acdb2374e9dea3e414e6643d794497893e4fb6
Lin <mt5718214@gmail.com>
2020-11-08 11:41:38 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,4 +5,12 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/login', (req, res) => {
+
+})
+
 module.exports = router
