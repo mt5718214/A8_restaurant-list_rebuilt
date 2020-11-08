8077d98ce9343bb02fd534ab13102353c59e7465
Lin <mt5718214@gmail.com>
2020-11-08 22:22:12 +0800

    feat: modify restaurantSeeder.js

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,22 +1,47 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const bcrypt = require('bcryptjs')
 const Restaurant = require('../restaurant')
-const restaurantSeedData = require('../../restaurant')
-const seedData = restaurantSeedData.results
+const User = require('../user')
+const restaurantSeedData = require('../../restaurant').results
 
 const db = require('../../config/mongoose')
 
+const SEED_USERS = [
+  {
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    email: 'user2@example.com',
+    password: '12345678'
+  }
+]
+
 db.once('open', () => {
-  seedData.forEach(restaurant => {
-    Restaurant.create({
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: restaurant.rating,
-      description: restaurant.description
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USERS[0].password, salt))
+    .then(hash => {
+      SEED_USERS.forEach(SEED_USER => SEED_USER.password = hash)
+      return User.insertMany(SEED_USERS)
     })
-  })
-  console.log('Done')
+    .then(user => {
+      // console.log(user)
+      return Promise.all(restaurantSeedData.map((restaurant, index) => {
+        if (index < 3) {
+          restaurant.userId = user[0]._id
+          return Restaurant.create(Object.assign({}, restaurant))
+        }
+        if (index < 6) {
+          restaurant.userId = user[1]._id
+          return Restaurant.create(Object.assign({}, restaurant))
+        }
+      }))
+    })
+    .then(() => {
+      console.log('Done')
+      process.exit()
+    })
 })
